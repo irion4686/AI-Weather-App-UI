232519ebf26451602cb5148db7a622047b5449be
irion4686 <67257689+irion4686@users.noreply.github.com>
2024-07-09 10:00:16 -0400

    Cody sprint1 (#1)
    * Added util function to make GET and POST calls
    
    * Added util function to make GET and POST calls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect } from 'react';
+import ApiUtils from './Utils/ApiUtils';
 function App() {
+  const apiUtils = new ApiUtils();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let body = {
+          zipcode: "28425"
+        }
+        const result = await apiUtils.post('/forecast/hourly', body);
+        console.log(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      try {
+        const result = await apiUtils.get('/forecast/28425');
+        console.log(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/Utils/ApiUtils.js
@@ -0,0 +1,51 @@
+﻿class ApiUtils {
+    constructor(baseURL) {
+        this.baseURL = process.env.REACT_APP_BACKEND_URL;
+    }
+
+    async get(endpoint, headers = {}) {
+        try {
+            const response = await fetch(`${this.baseURL}${endpoint}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...headers,
+                },
+            });
+            return await this.handleResponse(response);
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
+
+    async post(endpoint, data, headers = {}) {
+        try {
+            const response = await fetch(`${this.baseURL}${endpoint}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...headers,
+                },
+                body: JSON.stringify(data),
+            });
+            return await this.handleResponse(response);
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
+
+    async handleResponse(response) {
+        if (!response.ok) {
+            const error = await response.text();
+            throw new Error(error);
+        }
+        return response.json();
+    }
+
+    handleError(error) {
+        console.error('API call failed. ', error);
+        throw error;
+    }
+}
+
+export default ApiUtils;
